4f40f13a33d63fd26fca46c4f03161187b1df336
AHMED AFANDY <eng.computer1@yahoo.com>
2021-01-08 05:06:10 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -25,7 +25,7 @@ def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_th
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-  "SELECT  projects.category,SUM(pledges.amount) FROM projects INNER JOIN pledges ON pledges.project_id = projects.id GROUP BY projects.category 
+  "SELECT  projects.category,SUM(pledges.amount) FROM projects INNER JOIN pledges ON pledges.project_id = projects.id GROUP BY projects.category
   HAVING  projects.category = 'music';"
 
 end
