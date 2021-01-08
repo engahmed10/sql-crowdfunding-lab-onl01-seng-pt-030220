e5a2d9187def6bcfd74cdf206afa6bebdc9a5b4c
AHMED AFANDY <eng.computer1@yahoo.com>
2021-01-08 03:56:09 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -15,5 +15,5 @@ end
 
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-   "SELECT projects.title,SUM(funding_goal) FROM projects"
+   "SELECT projects.title,SUM(pledges.amount - projects.funding_goal) FROM projects"
 end
