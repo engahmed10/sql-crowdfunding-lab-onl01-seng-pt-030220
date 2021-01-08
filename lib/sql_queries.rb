6994182bc407ba74bd66402faed7d737892de842
AHMED AFANDY <eng.computer1@yahoo.com>
2021-01-08 02:23:12 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -5,3 +5,6 @@
 # end
 
 # Make sure each ruby method returns a string containing a valid SQL statement.
+def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_title
+  "SELCET projects.title FROM "
+end
