4860b101d40be0f338e8dc010f7a6184e867cc0d
AHMED AFANDY <eng.computer1@yahoo.com>
2021-01-08 05:12:07 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -26,6 +26,6 @@ end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
   "SELECT  projects.category,(pledges.amount) FROM projects INNER JOIN pledges ON pledges.project_id = projects.id GROUP BY projects.category
-  HAVING  projects.category = 'music';"
+  WHERE  projects.category = 'music';"
 
 end
